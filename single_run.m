c4e67312085426b1de76fc9a87c851cabc3db7bd
Dean Pearce <dapearce@live.unc.edu>
2023-02-28 10:46:19 -0500

    single_run sets parameters

---
+++ single_run.m
@@ -0,0 +1,8 @@
+a1 = 0; % value<1 % probability of infection for strain 1
+a2 = 0; % value<1 % probability of infection for strain 2
+b1 = 0; % value<1 % probability of recovery for strain 1
+b2 = 0; % value<1 % probability of recovery for strain 2
+t_intr = 0; % time of introduction of new strain
+t_end = 0; % time of end of simulation
+d = 1; % timescale dilator
+n = 100; % size of population
