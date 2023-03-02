d3f3a9fa4278de4366e432615f36d2eb55ae4062
Ivvone <72171853+ivvone@users.noreply.github.com>
2023-03-02 14:30:31 -0500

    Fixed mistakes, restored single_run.m and created parameter_traverse.m

--- single_run.m
+++ single_run.m
@@ -1,30 +1,13 @@
-% step/resolution 
-step = 0.1; 
- 
-% start/end of a2 parameter 
-a2_start = 0.1; 
-a2_end = 0.3; 
- 
-% start/end of b2 parameter 
-b2_start = 0.1; 
-b2_end = 0.3; 
+a1 = 0; % value<1 % probability of infection for strain 1
+a2 = 0; % value<1 % probability of infection for strain 2
+b1 = 0; % value<1 % probability of recovery for strain 1
+b2 = 0; % value<1 % probability of recovery for strain 2
+t_intr = 0; % time of introduction of new strain
+t_end = 0; % time of end of simulation
+d = 1; % timescale dilator
+n = 100; % size of population
 
-% empty matrix to hold response
-n_a2 = int16((a2_end - a2_start) / step);
-n_b2 = int16((b2_end - b2_start) / step);
-response = zeros(n_a2, n_b2);
- 
-% Traverse parameter space
-for i = 0:n_a2
-    for j = 0:n_b2
-        % a2 and b2 parameter values
-        a2 = a2_start + double(i)*step
-        b2 = b2_start + double(j)*step
-        out = rand();
-        response(i+1, j+1) = out;
-    end 
-end 
-
-%% Create heatmap based on matrix
-heatmap(response, 'Colormap',spring)
-title("[Example] Time to dominance")
+t_end = 0; % time of end of simulation
+d = 1; % timescale dilator
+n = 100; % size of population
+sir_model
